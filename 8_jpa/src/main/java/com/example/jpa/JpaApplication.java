351ce86a4cd6b6810511ce96536cd1b1e1571868
aquashdw <aquashdw@gmail.com>
2023-06-15 08:57:35 +0900

    remove comment to allow JpaApplication run

--- 8_jpa/src/main/java/com/example/jpa/JpaApplication.java
+++ 8_jpa/src/main/java/com/example/jpa/JpaApplication.java
@@ -3,9 +3,7 @@ package com.example.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigInteger;
-
-//@SpringBootApplication
+@SpringBootApplication
 public class JpaApplication {
 
 	public static void main(String[] args) {
