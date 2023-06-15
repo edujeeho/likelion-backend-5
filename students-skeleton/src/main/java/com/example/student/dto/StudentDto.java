25813d0f69c55efe60a0e1943080b7b5683173b5
kdybe01 <kdybe01@gmail.com>
2023-06-15 20:08:36 +0900

    Feat : Student 엔티티 작성

--- students-skeleton/src/main/java/com/example/student/dto/StudentDto.java
+++ students-skeleton/src/main/java/com/example/student/dto/StudentDto.java
@@ -1,4 +1,23 @@
 package com.example.student.dto;
 
+import com.example.student.entity.StudentEntity;
+import lombok.Data;
+
+@Data
 public class StudentDto {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String phone;
+    private String email;
+
+    public static StudentDto fromEntity(StudentEntity entity) {
+        StudentDto dto = new StudentDto();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        dto.setAge(entity.getAge());
+        dto.setPhone(entity.getPhone());
+        dto.setEmail(entity.getEmail());
+        return dto;
+    }
 }
