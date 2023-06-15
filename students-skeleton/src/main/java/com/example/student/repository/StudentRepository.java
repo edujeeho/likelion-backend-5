c2657c148d8bbafe263b7ef54829cd05d67e9312
edujeeho <jeeho.dev@gmail.com>
2023-06-15 16:04:58 +0900

    repository folder 남겨두기

--- students-skeleton/src/main/java/com/example/student/repository/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.student.repository;
-
-import com.example.student.entity.StudentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<StudentEntity, Long> { }
