c3317e76a3df8120302823725bf166c7d877a8d3
kdybe01 <kdybe01@gmail.com>
2023-06-15 20:08:57 +0900

    Feat : Student 컨트롤러 작성

--- students-skeleton/src/main/java/com/example/student/StudentController.java
+++ students-skeleton/src/main/java/com/example/student/StudentController.java
@@ -1,9 +1,11 @@
 package com.example.student;
 
+import com.example.student.dto.StudentDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,50 +14,63 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/students")
 public class StudentController {
 
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
     @GetMapping("")
     public String home(Model model) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+        model.addAttribute("studentList", service.readStudentAll());
+        return "home";
     }
 
     // create.html 응답
     @GetMapping("/create-view")
     public String createView() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+        return "create";
     }
 
     // 새로운 StudentEntity 생성 후 상세보기 페이지로
     @PostMapping("/create")
-    public String create() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String create(StudentDto dto) {
+        StudentDto newDto = service.createStudent(dto);
+        return "redirect:/students/" + newDto.getId();
     }
 
     // id에 해당하는 StudentEntity의 read.html 응답
     @GetMapping("/{id}")
-    public String read() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String read(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("student", service.readStudent(id));
+        return "read";
     }
 
     // id에 해당하는 StudentEntity의 update.html 응답
     @GetMapping("/{id}/update-view")
-    public String updateView(){
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String updateView(@PathVariable("id") Long id, Model model){
+        model.addAttribute("student", service.readStudent(id));
+        return "update";
     }
 
     // id에 해당하는 StudentEntity 수정 후 상세보기 페이지로
     @PostMapping("/{id}/update")
-    public String update() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String update(@PathVariable("id") Long id, StudentDto dto) {
+        StudentDto updateStudent = service.updateStudent(id, dto);
+        return "redirect:/students/" + updateStudent.getId();
     }
 
     // id에 해당하는 StudentEntity delete.html
     @GetMapping("/{id}/delete-view")
-    public String deleteView() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String deleteView(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("student", service.readStudent(id));
+        return "delete";
     }
 
     // id에 해당하는 StudentEntity 삭제 후 홈페이지로
     @PostMapping("/{id}/delete")
-    public String delete() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public String delete(@PathVariable("id") Long id) {
+        service.deleteStudent(id);
+        return "redirect:/students";
     }
 }
